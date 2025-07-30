6d86aed4dc9b67968508dbcbdb05d7ecc0c01e60
RamzanKhan <ramzanktk2024>
2025-07-30 03:03:18 -0700

    make add to cart button anteractive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,30 +1,3 @@
-const products = [{
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: " Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating:{
-        stars: 4.5,
-        counts: 87
-    },
-    priceCents: 1090
-},
-{
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: " Intermediate Size Basketball",
-    rating:{
-        stars: 4,
-        counts: 127
-    },
-    priceCents: 2095
-},
-{
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating:{
-        stars: 4.5,
-        counts: 56
-    },
-    priceCents: 799
-}];
 
 let productHTML = '';
 products.forEach((product) => {
@@ -72,10 +45,32 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`;
 
 });
 document.querySelector(".js-products-grid").innerHTML = productHTML;
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId){
+        matchingItem = item;
+      }
+    });
+     if(matchingItem){
+        matchingItem.quantity += 1;
+      }
+      else{
+          cart.push({
+           productId,
+            quantity: 1
+        });
+      }
+  console.log(cart)
+  });
+});
