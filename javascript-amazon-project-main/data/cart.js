75389d219d9bdb644ba58eb1fc36abc876bc5ce5
RamzanKhan <ramzanktk2024>
2025-08-03 20:10:01 -0700

    save cart to local storage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,11 +1,14 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2
 }, {
   productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
   quantity: 1
 }]; 
-
+function saveToStorage()
+{
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 export function addtoCart(productId) {
   let matchingItem;
     cart.forEach((cartItem) => {
@@ -25,6 +28,7 @@ export function addtoCart(productId) {
             quantity
         });
       }
+      saveToStorage();
 }
 export function updateCartQuantity(){
       let cartQuantity = 0;
@@ -43,4 +47,5 @@ export function removeItemFromCart(productId) {
     }
   });
   cart = newCart;
+  saveToStorage();
 }
