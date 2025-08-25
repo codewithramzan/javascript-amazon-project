b20585f24bcd1ecfc5b843bd6a471a80e90fd73c
RamzanKhan <ramzanktk2024>
2025-08-25 02:24:10 -0700

    create class version cart

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderCheckoutHeader } from "./Checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./Checkout/order-summary.js";
 import { renderPaymentSummary } from "./Checkout/payment-summary.js";
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 
 renderCheckoutHeader();
